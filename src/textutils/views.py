c8b344389233b459bc51431a4e852e9068edee1e
Shishir <sishishir2015@gmail.com>
2020-02-25 16:19:29 +0600

    New Line Remover and Uppercase Feature Added

--- src/textutils/views.py
+++ src/textutils/views.py
@@ -10,11 +10,15 @@ def index(request):
 
 
 def analyze(request):
-    #Get The Text:
+    #  Get The Text:
     djtext = request.GET.get('text', 'default')
-    removepunctuation = request.GET.get('removepunctuation', 'default')
-    print(removepunctuation)
-    print(djtext)
+
+    # CheckBox Values:
+    removepunctuation = request.GET.get('removepunctuation', 'off')
+    caps = request.GET.get('caps','off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    # print(removepunctuation)
+    # print(djtext)
 
     # Analyze The Text"
     if removepunctuation== "on":
@@ -25,8 +29,29 @@ def analyze(request):
                 analyze = analyze + char
 
         params = {'purpose': 'Remove Punctuation', 'analyzed_text': analyze}
-        #return HttpResponse('rempunc')
+        # return HttpResponse('rempunc')
         return render(request, 'analyzer.html', params)
+
+    elif(caps== "on"):
+        analyze= ""
+
+        for char in djtext:
+            analyze = analyze + char.upper()
+
+        params = {'purpose': 'All Uppercase', 'analyzed_text': analyze}
+        # return HttpResponse('rempunc')
+        return render(request, 'analyzer.html', params)
+
+    elif(newlineremover == "on"):
+
+        analyze=""
+
+        for char in djtext:
+            if char != "\n":
+                analyze = analyze + char
+
+        params = {'purpose':"New Line Removed",'analyzed_text':analyze}
+        return render(request,'analyzer.html', params)
 
     else:
         return HttpResponse("Error")
@@ -44,9 +69,24 @@ def analyze(request):
 #     #return render(request, 'removepunc.html')
 #
 #
-# def capfirst(request):
-#     return render(request, 'capitalfirst.html')
+#   def capfirst(request):
 #
+#       if capfirst == 'on':
+#
+#           djtext=request.GET.get('text','default')
+#
+#           capitfirst = ""
+#
+#           for char in djtext:
+#               if
+#
+#           pms = {'purpose2':"Capital First Letter","cap-first":capfirst}
+#
+#           return render(request, 'capitalfirst.html',pms)
+#
+#       else:
+#             return HttpResponse("Error")
+# #
 #
 # def newlinerem(request):
 #     return render(request, 'newlinere.html')
